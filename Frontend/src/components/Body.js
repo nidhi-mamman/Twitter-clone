35ab08f45f9f49c878046b2f6bbbd8054955b193
nidhimamman3 <nidhimamman3@gmail.com>
2024-10-05 23:05:14 +0530

    minors

--- Frontend/src/components/Body.js
+++ Frontend/src/components/Body.js
@@ -1,61 +1,37 @@
-// import React from 'react'
-// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-// import Home from './Home'
-// import Profile from './Profile'
-// import Feed from './Feed'
-// import Login from './Login'
-// // import { Bookmarks } from ''
-// const Body = () => {
-//     const userbrowser=createBrowserRouter([
-//         {
-//             path:"/",
-//             element:<Home/>,
-//             children:[
-//               {
-//                 path:'/',
-//                 element:<Feed/>
-//               },
-//               {
-//                 path:'/profile/:id',
-//                 element:<Profile/>
-//               }
-//             ]
-
-//         },
-//         {
-//             path:"/login",
-//             element:<Login/>    
-//         }
-//     ])
-//   return (
-//     <div>
-//       <RouterProvider router={userbrowser}/>
-//     </div>
-//   )
-// }
-
-// export default Body
-import React from 'react';
-import {  Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
-import Feed from './Feed';
-import Login from './Login';
-
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './Home'
+import Profile from './Profile'
+import Feed from './Feed'
+import Login from './Login'
+// import { Bookmarks } from ''
 const Body = () => {
+    const userbrowser=createBrowserRouter([
+        {
+            path:"/",
+            element:<Home/>,
+            children:[
+              {
+                path:'/',
+                element:<Feed/>
+              },
+              {
+                path:'/profile/:id',
+                element:<Profile/>
+              }
+            ]
+
+        },
+        {
+            path:"/login",
+            element:<Login/>    
+        }
+    ])
   return (
-      <Routes>
-        {/* Main Home route */}
-        <Route path="/" element={<Home />}>
-          {/* Child routes under Home */}
-          <Route index element={<Feed />} />  {/* This acts as the default route under Home */}
-          <Route path="profile/:id" element={<Profile />} />
-        </Route>
+    <div>
+      <RouterProvider router={userbrowser}/>
+    </div>
+  )
+}
 
-        {/* Login route */}
-        <Route path="/login" element={<Login />} />
-      </Routes>
-  );
-};
-
-export default Body;
+export default Body
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './redux/store';
-import { BrowserRouter as Router} from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from "redux-persist";
 // import reportWebVitals from './reportWebVitals';
@@ -14,9 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
         <App />
-        </Router>
       </PersistGate>
     </Provider >
   </React.StrictMode>
